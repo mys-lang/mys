0f5126e4c4c3e878b022ed97a0b520df750853cb
Erik Moqvist <erik.moqvist@gmail.com>
2020-08-21 16:54:12 +0200

    Added the run subcommand.

--- mys/__init__.py
+++ mys/__init__.py
@@ -1,5 +1,9 @@
-from .version import __version__
+import sys
+import argparse
 from typing import Tuple
+
+from .version import __version__
+
 
 u8 = int
 u16 = int
@@ -14,5 +18,38 @@ f64 = float
 
 Queue = Tuple
 
+
+def _do_run(args):
+    raise NotImplementedError('The run subcommand is not yet implemented.')
+
+
 def main():
-    raise NotImplementedError()
+    parser = argparse.ArgumentParser(
+        description='The Mys programming language command line tool.')
+
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('--version',
+                        action='version',
+                        version=__version__,
+                        help='Print version information and exit.')
+
+    # Workaround to make the subparser required in Python 3.
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       dest='subcommand')
+    subparsers.required = True
+
+    # The run subparser.
+    subparser = subparsers.add_parser(
+        'run',
+        description='Run the program.')
+    subparser.set_defaults(func=_do_run)
+
+    args = parser.parse_args()
+
+    if args.debug:
+        args.func(args)
+    else:
+        try:
+            args.func(args)
+        except BaseException as e:
+            sys.exit('error: ' + str(e))
