7cdab3ef197941cdbcb929383b7181ced3aa7723
Erik Moqvist <erik.moqvist@gmail.com>
2020-08-23 11:28:47 +0200

    Cleanup.

--- tests/files/test_re.mys.cpp
+++ tests/files/test_re.mys.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
-#include <string>
 #include <regex>
+#include "mys.hpp"
 
 int main()
 {
     std::string sub("subject4");
     std::regex re("(sub)(.*)(\\d)");
     std::smatch groups;
-
+    const char *delim_p;
+    unsigned i;
+        
     if (std::regex_match(sub, groups, re)) {
         std::cout << "Groups: (";
 
-        for (unsigned i = 1; i < groups.size(); i++) {
-            std::cout << (i == 1 ? "'" : ", '") << groups[i] << "'";
+        for (i = 1, delim_p = "'"; i < groups.size(); i++, delim_p = ", '") {
+            std::cout << delim_p << groups[i] << "'";
         }
 
         std::cout << ")" << std::endl;
