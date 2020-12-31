ec9cfc8dd574e1fbc8199bb0e4e6fd0eca14e74d
Erik Moqvist <erik.moqvist@gmail.com>
2020-12-31 09:41:10 +0100

    Refactoring.

--- mys/transpile/imports_visitor.py
+++ mys/transpile/imports_visitor.py
@@ -6,15 +6,15 @@ class ImportsVisitor(ast.NodeVisitor):
     def __init__(self):
         self._other_found = False
 
+    def visit_Module(self, node):
+        body = [
+            self.visit(item)
+            for item in node.body
+        ]
+
     def visit_ImportFrom(self, node):
         if self._other_found:
             raise CompileError("imports must be at the beginning of the file", node)
 
-    def visit_ClassDef(self, node):
-        self._other_found = True
-
-    def visit_AnnAssign(self, node):
-        self._other_found = True
-
-    def visit_FunctionDef(self, node):
+    def generic_visit(self, node):
         self._other_found = True
