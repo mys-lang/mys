66c3bfd629b217d1dc18f58f67bd2a31335dc51f
Erik Moqvist <erik.moqvist@gmail.com>
2020-12-31 09:42:40 +0100

    Documentation.

--- mys/transpile/imports_visitor.py
+++ mys/transpile/imports_visitor.py
@@ -2,15 +2,17 @@ from ..parser import ast
 from .utils import CompileError
 
 class ImportsVisitor(ast.NodeVisitor):
+    """Raises an error if not all imports are at the beginning of the
+    file.
+
+    """
 
     def __init__(self):
         self._other_found = False
 
     def visit_Module(self, node):
-        body = [
+        for item in node.body:
             self.visit(item)
-            for item in node.body
-        ]
 
     def visit_ImportFrom(self, node):
         if self._other_found:
