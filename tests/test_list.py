92f22987086ab402e85a35547c72b55e1329325b
Erik Moqvist <erik.moqvist@gmail.com>
2021-01-08 21:27:58 +0100

    Move list tests to test_list.py.

--- tests/test_list.py
+++ tests/test_list.py
@@ -11,3 +11,100 @@ class Test(TestCase):
 
     def test_list(self):
         build_and_test_module('list')
+
+    def test_type_error_4(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('def foo():\n'
+                             '    a: u32 = [1.0]\n'
+                             '    print(a)\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        a: u32 = [1.0]\n'
+            '                 ^\n'
+            "CompileError: cannot convert list to 'u32'\n")
+
+    def test_return_wrong_list_type_1(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('def foo() -> [u8]:\n'
+                             '    return [i8(1), -1]\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        return [i8(1), -1]\n'
+            '                ^\n'
+            "CompileError: expected a 'u8', got a 'i8'\n")
+
+    def test_return_wrong_list_type_2(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('def foo() -> [u8]:\n'
+                             '    return [1, i8(-1)]\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        return [1, i8(-1)]\n'
+            '                   ^\n'
+            "CompileError: expected a 'u8', got a 'i8'\n")
+
+    def test_list_of_integer(self):
+        with self.assertRaises(Exception) as cm:
+            source = transpile_source('def foo():\n'
+                                      '    a = list("")\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        a = list("")\n'
+            '            ^\n'
+            "CompileError: list('string') not supported\n")
+
+    def test_class_member_list_two_types(self):
+        with self.assertRaises(Exception) as cm:
+            source = transpile_source('class Foo:\n'
+                                      '    a: [i32, u32]\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        a: [i32, u32]\n'
+            '           ^\n'
+            "CompileError: expected 1 type in list, got 2\n")
+
+    def test_list_comprehension(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('def foo():\n'
+                             '    print([v for v in ""])\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        print([v for v in ""])\n'
+            '              ^\n'
+            "CompileError: list comprehension is not implemented\n")
+
+    def test_define_empty_list_without_type(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('def foo():\n'
+                             '    v = []\n'
+                             '    print(v)\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '        v = []\n'
+            '        ^\n'
+            "CompileError: cannot infer type from empty list\n")
+
+    def test_list_with_two_types(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('VAR: [bool, bool] = None\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 1\n'
+            '    VAR: [bool, bool] = None\n'
+            '         ^\n'
+            "CompileError: expected 1 type in list, got 2\n")
--- tests/test_mys.py
+++ tests/test_mys.py
@@ -1683,19 +1683,6 @@ class Test(TestCase):
             '            ^\n'
             "CompileError: types 'u32' and '[i64]' differs\n")
 
-    def test_type_error_4(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('def foo():\n'
-                             '    a: u32 = [1.0]\n'
-                             '    print(a)\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        a: u32 = [1.0]\n'
-            '                 ^\n'
-            "CompileError: cannot convert list to 'u32'\n")
-
     def test_type_error_5(self):
         with self.assertRaises(Exception) as cm:
             transpile_source('def foo() -> bool:\n'
@@ -2487,30 +2474,6 @@ class Test(TestCase):
             '                      ^\n'
             "CompileError: expected a 'u8', got a 'i8'\n")
 
-    def test_return_wrong_list_type_1(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('def foo() -> [u8]:\n'
-                             '    return [i8(1), -1]\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        return [i8(1), -1]\n'
-            '                ^\n'
-            "CompileError: expected a 'u8', got a 'i8'\n")
-
-    def test_return_wrong_list_type_2(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('def foo() -> [u8]:\n'
-                             '    return [1, i8(-1)]\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        return [1, i8(-1)]\n'
-            '                   ^\n'
-            "CompileError: expected a 'u8', got a 'i8'\n")
-
     def test_return_none_as_bool(self):
         with self.assertRaises(Exception) as cm:
             transpile_source('def foo() -> bool:\n'
@@ -2777,30 +2740,6 @@ class Test(TestCase):
             '                 ^\n'
             "CompileError: class members cannot have default values\n")
 
-    def test_list_of_integer(self):
-        with self.assertRaises(Exception) as cm:
-            source = transpile_source('def foo():\n'
-                                      '    a = list("")\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        a = list("")\n'
-            '            ^\n'
-            "CompileError: list('string') not supported\n")
-
-    def test_class_member_list_two_types(self):
-        with self.assertRaises(Exception) as cm:
-            source = transpile_source('class Foo:\n'
-                                      '    a: [i32, u32]\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        a: [i32, u32]\n'
-            '           ^\n'
-            "CompileError: expected 1 type in list, got 2\n")
-
     def test_trait_init(self):
         with self.assertRaises(Exception) as cm:
             source = transpile_source('@trait\n'
@@ -3036,18 +2975,6 @@ class Test(TestCase):
             '        print(1 if True else "")\n'
             '                             ^\n'
             "CompileError: expected a 'i64', got a 'string'\n")
-
-    def test_list_comprehension(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('def foo():\n'
-                             '    print([v for v in ""])\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        print([v for v in ""])\n'
-            '              ^\n'
-            "CompileError: list comprehension is not implemented\n")
 
     def test_if_cond_as_non_bool(self):
         with self.assertRaises(Exception) as cm:
@@ -3442,19 +3369,6 @@ class Test(TestCase):
             '        print(v)\n'
             '              ^\n'
             "CompileError: undefined variable 'v'\n")
-
-    def test_define_empty_list_without_type(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('def foo():\n'
-                             '    v = []\n'
-                             '    print(v)\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '        v = []\n'
-            '        ^\n'
-            "CompileError: cannot infer type from empty list\n")
 
     def test_define_empty_dict_without_type(self):
         with self.assertRaises(Exception) as cm:
@@ -3870,17 +3784,6 @@ class Test(TestCase):
 
         self.assert_in('// nothing 1', source)
         self.assert_in('// nothing 2', source)
-
-    def test_list_with_two_types(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('VAR: [bool, bool] = None\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 1\n'
-            '    VAR: [bool, bool] = None\n'
-            '         ^\n'
-            "CompileError: expected 1 type in list, got 2\n")
 
     def test_complex(self):
         # complex may be implemented at some point.
