483594d3a05b3cfcc0b5f9dad9176f04f89ef7d7
Erik Moqvist <erik.moqvist@gmail.com>
2021-01-08 21:02:42 +0100

    Added disabled generic type test as it do not pass yet.

--- tests/test_mys.py
+++ tests/test_mys.py
@@ -3913,3 +3913,18 @@ class Test(TestCase):
             '    VAR: complex = 1 + 2j\n'
             '         ^\n'
             "CompileError: undefined type 'complex'\n")
+    # ToDo
+    # def test_genric_undefined_type(self):
+    #     with self.assertRaises(Exception) as cm:
+    #         transpile_source('@generic(T)\n'
+    #                          'def add(a: T) -> T:\n'
+    #                          '    return a\n'
+    #                          'def foo():\n'
+    #                          '    print(add[Foo](None))\n')
+    #
+    #     self.assertEqual(
+    #         remove_ansi(str(cm.exception)),
+    #         '  File "", line 1\n'
+    #         '    VAR: complex = 1 + 2j\n'
+    #         '         ^\n'
+    #         "CompileError: undefined type 'complex'\n")
