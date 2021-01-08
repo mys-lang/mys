cdcdf6f23d2d1becaaa9c67519879f1fd6b2d960
Erik Moqvist <erik.moqvist@gmail.com>
2021-01-08 21:55:42 +0100

    Move tests to test_string and test_traits.py.

--- tests/test_mys.py
+++ tests/test_mys.py
@@ -714,84 +714,6 @@ class Test(TestCase):
             '     ^\n'
             "CompileError: decorators must be @name or @name()\n")
 
-    def test_define_empty_trait(self):
-        header = transpile_early_header('@trait\n'
-                                        'class Foo:\n'
-                                        '    pass\n')
-        self.assert_in('class Foo : public Object {\n'
-                       'public:\n'
-                       '};\n',
-                       header)
-
-    def test_define_trait_with_member(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Foo:\n'
-                             '    a: i32\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 3\n'
-            '        a: i32\n'
-            '        ^\n'
-            "CompileError: traits cannot have members\n")
-
-    def test_define_trait_with_parameter(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait(u32)\n'
-                             'class Foo:\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 1\n'
-            '    @trait(u32)\n'
-            '     ^\n'
-            "CompileError: no parameters expected\n")
-
-    def test_define_trait_with_same_name_twice(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Foo:\n'
-                             '    pass\n'
-                             '@trait\n'
-                             'class Foo:\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 5\n'
-            '    class Foo:\n'
-            '    ^\n'
-            "CompileError: there is already a trait called 'Foo'\n")
-
-    def test_define_trait_with_same_name_as_class(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('class Foo:\n'
-                             '    pass\n'
-                             '@trait\n'
-                             'class Foo:\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 4\n'
-            '    class Foo:\n'
-            '    ^\n'
-            "CompileError: there is already a class called 'Foo'\n")
-
-    def test_trait_does_not_exist(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('class Foo(Bar):\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 1\n'
-            '    class Foo(Bar):\n'
-            '              ^\n'
-            "CompileError: trait does not exist\n")
-
     def test_invalid_decorator(self):
         with self.assertRaises(Exception) as cm:
             transpile_source('@foobar\n'
@@ -955,29 +877,6 @@ class Test(TestCase):
             '        foo(True)\n'
             '            ^\n'
             "CompileError: expected a 'string', got a 'bool'\n")
-
-    def test_wrong_function_parameter_type_trait(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Base:\n'
-                             '    pass\n'
-                             '@trait\n'
-                             'class WrongBase:\n'
-                             '    pass\n'
-                             'class Foo(Base):\n'
-                             '    pass\n'
-                             'def foo(a: WrongBase):\n'
-                             '    pass\n'
-                             'def bar():\n'
-                             '    foo(Foo())\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 12\n'
-            '        foo(Foo())\n'
-            '            ^\n'
-            "CompileError: 'foo.lib.Foo' does not implement trait "
-            "'foo.lib.WrongBase'\n")
 
     def test_wrong_method_parameter_type(self):
         with self.assertRaises(Exception) as cm:
@@ -2400,88 +2299,6 @@ class Test(TestCase):
             '                 ^\n'
             "CompileError: class members cannot have default values\n")
 
-    def test_trait_init(self):
-        with self.assertRaises(Exception) as cm:
-            source = transpile_source('@trait\n'
-                                      'class Foo:\n'
-                                      '    def __init__(self):\n'
-                                      '        pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 3\n'
-            '        def __init__(self):\n'
-            '        ^\n'
-            "CompileError: traits cannot have an __init__ method\n")
-
-    def test_trait_method_not_implemented_1(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Base:\n'
-                             '    def foo(self):\n'
-                             '        pass\n'
-                             'class Foo(Base):\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 5\n'
-            '    class Foo(Base):\n'
-            '              ^\n'
-            "CompileError: trait method 'foo' is not implemented\n")
-
-    def test_trait_method_not_implemented_2(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Base:\n'
-                             '    def foo(self):\n'
-                             '        "Doc"\n'
-                             'class Foo(Base):\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 5\n'
-            '    class Foo(Base):\n'
-            '              ^\n'
-            "CompileError: trait method 'foo' is not implemented\n")
-
-    # ToDo
-    # def test_imported_traits_method_not_implemented(self):
-    #     with self.assertRaises(Exception) as cm:
-    #         transpile([
-    #             Source('@trait\n'
-    #                    'class Base:\n'
-    #                    '    def foo(self):\n'
-    #                    '        pass\n',
-    #                    module='foo.lib'),
-    #             Source('from foo import Base\n'
-    #                    'class Foo(Base):\n'
-    #                    '    pass\n')
-    #         ])
-    #
-    #     self.assertEqual(
-    #         remove_ansi(str(cm.exception)),
-    #         '  File "", line 2\n'
-    #         '    class Foo(Base):\n'
-    #         '              ^\n'
-    #         "CompileError: trait method 'foo' is not implemented\n")
-
-    def test_trait_member_access(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Base:\n'
-                             '    pass\n'
-                             'def foo(v: Base):\n'
-                             '    v.a = 1\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 5\n'
-            '        v.a = 1\n'
-            '        ^\n'
-            "CompileError: 'Base' has no member 'a'\n")
-
     def test_main_in_non_main_file(self):
         with self.assertRaises(Exception) as cm:
             transpile_source('def main():\n'
@@ -2496,21 +2313,6 @@ class Test(TestCase):
                              has_main=True)
 
         self.assertEqual(str(cm.exception), 'main() not found in main.mys')
-
-    def test_trait_member_access(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('class Foo:\n'
-                             '    def foo(self, v: bool):\n'
-                             '        pass\n'
-                             'def foo(v: Foo):\n'
-                             '    v.foo()\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 5\n'
-            '        v.foo()\n'
-            '        ^\n'
-            "CompileError: expected 1 parameter, got 0\n")
 
     def test_call_void(self):
         with self.assertRaises(Exception) as cm:
@@ -2591,22 +2393,6 @@ class Test(TestCase):
             '        v.foo("")\n'
             '              ^\n'
             "CompileError: expected a 'bool', got a 'string'\n")
-
-    def test_wrong_trait_method_parameter_type(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Foo:\n'
-                             '    def foo(self, v: bool):\n'
-                             '        pass\n'
-                             'def foo(v: Foo):\n'
-                             '    v.foo(b"")\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 6\n'
-            '        v.foo(b"")\n'
-            '              ^\n'
-            "CompileError: expected a 'bool', got a 'bytes'\n")
 
     def test_bare_compare(self):
         with self.assertRaises(Exception) as cm:
@@ -2818,19 +2604,6 @@ class Test(TestCase):
             '        print(+Foo())\n'
             '              ^\n'
             "CompileError: unary '+' can only operate on numbers\n")
-
-    def test_trait_pascal_case(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class foo:\n'
-                             '    pass\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 2\n'
-            '    class foo:\n'
-            '    ^\n'
-            "CompileError: trait names must be pascal case\n")
 
     def test_variable_defined_in_if_can_not_be_used_after(self):
         with self.assertRaises(Exception) as cm:
@@ -3159,19 +2932,6 @@ class Test(TestCase):
             '    import bar\n'
             '    ^\n'
             "CompileError: only 'from <module> import ...' is allowed\n")
-
-    def test_trait_member(self):
-        with self.assertRaises(Exception) as cm:
-            transpile_source('@trait\n'
-                             'class Foo:\n'
-                             '    x: i32\n')
-
-        self.assertEqual(
-            remove_ansi(str(cm.exception)),
-            '  File "", line 3\n'
-            '        x: i32\n'
-            '        ^\n'
-            "CompileError: traits cannot have members\n")
 
     def test_inline_constant_default_bool_parameter_value(self):
         source = transpile_source('def foo(a: bool = True):\n'
--- tests/test_traits.py
+++ tests/test_traits.py
@@ -2,6 +2,9 @@ import shutil
 import os
 from .utils import build_and_test_module
 from .utils import TestCase
+from .utils import transpile_early_header
+from .utils import transpile_source
+from .utils import remove_ansi
 
 
 class Test(TestCase):
@@ -11,3 +14,243 @@ class Test(TestCase):
 
     def test_traits(self):
         build_and_test_module('traits')
+
+    def test_define_empty_trait(self):
+        header = transpile_early_header('@trait\n'
+                                        'class Foo:\n'
+                                        '    pass\n')
+        self.assert_in('class Foo : public Object {\n'
+                       'public:\n'
+                       '};\n',
+                       header)
+
+    def test_define_trait_with_member(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Foo:\n'
+                             '    a: i32\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 3\n'
+            '        a: i32\n'
+            '        ^\n'
+            "CompileError: traits cannot have members\n")
+
+    def test_define_trait_with_parameter(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait(u32)\n'
+                             'class Foo:\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 1\n'
+            '    @trait(u32)\n'
+            '     ^\n'
+            "CompileError: no parameters expected\n")
+
+    def test_define_trait_with_same_name_twice(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Foo:\n'
+                             '    pass\n'
+                             '@trait\n'
+                             'class Foo:\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 5\n'
+            '    class Foo:\n'
+            '    ^\n'
+            "CompileError: there is already a trait called 'Foo'\n")
+
+    def test_define_trait_with_same_name_as_class(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('class Foo:\n'
+                             '    pass\n'
+                             '@trait\n'
+                             'class Foo:\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 4\n'
+            '    class Foo:\n'
+            '    ^\n'
+            "CompileError: there is already a class called 'Foo'\n")
+
+    def test_trait_does_not_exist(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('class Foo(Bar):\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 1\n'
+            '    class Foo(Bar):\n'
+            '              ^\n'
+            "CompileError: trait does not exist\n")
+
+    def test_wrong_function_parameter_type_trait(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Base:\n'
+                             '    pass\n'
+                             '@trait\n'
+                             'class WrongBase:\n'
+                             '    pass\n'
+                             'class Foo(Base):\n'
+                             '    pass\n'
+                             'def foo(a: WrongBase):\n'
+                             '    pass\n'
+                             'def bar():\n'
+                             '    foo(Foo())\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 12\n'
+            '        foo(Foo())\n'
+            '            ^\n'
+            "CompileError: 'foo.lib.Foo' does not implement trait "
+            "'foo.lib.WrongBase'\n")
+
+    def test_trait_init(self):
+        with self.assertRaises(Exception) as cm:
+            source = transpile_source('@trait\n'
+                                      'class Foo:\n'
+                                      '    def __init__(self):\n'
+                                      '        pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 3\n'
+            '        def __init__(self):\n'
+            '        ^\n'
+            "CompileError: traits cannot have an __init__ method\n")
+
+    def test_trait_method_not_implemented_1(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Base:\n'
+                             '    def foo(self):\n'
+                             '        pass\n'
+                             'class Foo(Base):\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 5\n'
+            '    class Foo(Base):\n'
+            '              ^\n'
+            "CompileError: trait method 'foo' is not implemented\n")
+
+    def test_trait_method_not_implemented_2(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Base:\n'
+                             '    def foo(self):\n'
+                             '        "Doc"\n'
+                             'class Foo(Base):\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 5\n'
+            '    class Foo(Base):\n'
+            '              ^\n'
+            "CompileError: trait method 'foo' is not implemented\n")
+
+    # ToDo
+    # def test_imported_traits_method_not_implemented(self):
+    #     with self.assertRaises(Exception) as cm:
+    #         transpile([
+    #             Source('@trait\n'
+    #                    'class Base:\n'
+    #                    '    def foo(self):\n'
+    #                    '        pass\n',
+    #                    module='foo.lib'),
+    #             Source('from foo import Base\n'
+    #                    'class Foo(Base):\n'
+    #                    '    pass\n')
+    #         ])
+    #
+    #     self.assertEqual(
+    #         remove_ansi(str(cm.exception)),
+    #         '  File "", line 2\n'
+    #         '    class Foo(Base):\n'
+    #         '              ^\n'
+    #         "CompileError: trait method 'foo' is not implemented\n")
+
+    def test_trait_member_access_1(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Base:\n'
+                             '    pass\n'
+                             'def foo(v: Base):\n'
+                             '    v.a = 1\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 5\n'
+            '        v.a = 1\n'
+            '        ^\n'
+            "CompileError: 'foo.lib.Base' has no member 'a'\n")
+
+    def test_trait_member_access_2(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('class Foo:\n'
+                             '    def foo(self, v: bool):\n'
+                             '        pass\n'
+                             'def foo(v: Foo):\n'
+                             '    v.foo()\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 5\n'
+            '        v.foo()\n'
+            '        ^\n'
+            "CompileError: expected 1 parameter, got 0\n")
+
+    def test_wrong_trait_method_parameter_type(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Foo:\n'
+                             '    def foo(self, v: bool):\n'
+                             '        pass\n'
+                             'def foo(v: Foo):\n'
+                             '    v.foo(b"")\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 6\n'
+            '        v.foo(b"")\n'
+            '              ^\n'
+            "CompileError: expected a 'bool', got a 'bytes'\n")
+
+    def test_trait_pascal_case(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class foo:\n'
+                             '    pass\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 2\n'
+            '    class foo:\n'
+            '    ^\n'
+            "CompileError: trait names must be pascal case\n")
+
+    def test_trait_member(self):
+        with self.assertRaises(Exception) as cm:
+            transpile_source('@trait\n'
+                             'class Foo:\n'
+                             '    x: i32\n')
+
+        self.assertEqual(
+            remove_ansi(str(cm.exception)),
+            '  File "", line 3\n'
+            '        x: i32\n'
+            '        ^\n'
+            "CompileError: traits cannot have members\n")
