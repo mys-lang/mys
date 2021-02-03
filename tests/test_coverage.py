b52a8fb8ae8ec24d07abeec8d09189199cb7ed38
Erik Moqvist <erik.moqvist@gmail.com>
2021-02-03 12:37:21 +0100

    More coverage testing.

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -9,5 +9,15 @@ class Test(TestCase):
     def test_coverage(self):
         build_and_test_module('coverage', ['--coverage'])
 
+        with open('tests/build/test_coverage/.mys-coverage.txt', 'r') as fin:
+            mys_coverage = fin.read()
+
+        self.assert_in(
+            'File: ./src/coverage.mys\n'
+            '1 0\n'
+            '4 2\n'
+            '8 1\n',
+            mys_coverage)
+
         self.assertTrue(
             os.path.exists('tests/build/test_coverage/covhtml/index.html'))
