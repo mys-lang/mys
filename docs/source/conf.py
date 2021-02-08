28232cc7e885c6592453d4daac44b7ed9c135f09
Erik Moqvist <erik.moqvist@gmail.com>
2021-02-08 09:21:19 +0100

    Remove mys.sphinx as docs extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-    'mys.sphinx'
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
