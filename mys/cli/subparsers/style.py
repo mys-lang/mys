6c4652ef749b5145c38c025eef83a77778d1a4fd
Erik Moqvist <erik.moqvist@gmail.com>
2021-02-21 19:31:34 +0100

    Style subcommand started.

--- mys/cli/subparsers/style.py
+++ mys/cli/subparsers/style.py
@@ -1,14 +1,42 @@
+import glob
+
+from ...parser import ast
 from ..utils import ERROR
 from ..utils import box_print
 from ..utils import read_package_configuration
 
 
-def do_style(_parser, _args, _mys_config):
+class CommentsFinder(ast.NodeVisitor):
+    """Look for comments in source code not spanned by AST nodes.
+
+    """
+
+    def __init__(self, source):
+        self.comments = {}
+        self.source_lines = source.split('\n')
+        self.lineno = 1
+        self.col_offset = 0
+
+    def visit_FunctionDef(self, node):
+        print(node.lineno, node.col_offset)
+
+
+def do_style(_parser, args, _mys_config):
     read_package_configuration()
 
-    box_print(['This subcommand is not yet implemented.'], ERROR)
+    if not args.experimental:
+        box_print(['This subcommand is not yet implemented.'], ERROR)
 
-    raise Exception()
+        raise Exception()
+
+    for src in glob.glob('**/*.mys', recursive=True):
+        with open(src, 'r') as fin:
+            source = fin.read()
+
+        print(source)
+        tree = ast.parse(source)
+        print(ast.dump(tree, indent=4))
+        CommentsFinder(source).visit(tree)
 
 
 def add_subparser(subparsers):
@@ -17,4 +45,5 @@ def add_subparser(subparsers):
         description=(
             'Check that the package follows the Mys style guidelines. Automatically '
             'fixes trivial errors and prints the rest.'))
+    subparser.add_argument('-e', '--experimental', action='store_true')
     subparser.set_defaults(func=do_style)
