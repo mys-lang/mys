56922910a48743803f85806cf51d14501fffac7b
Erik Moqvist <erik.moqvist@gmail.com>
2021-04-18 06:47:41 +0200

    Website styling.

--- tests/files/foo/doc/conf.py
+++ tests/files/foo/doc/conf.py
@@ -52,6 +52,10 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'style_nav_header_background': '#7289da'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
